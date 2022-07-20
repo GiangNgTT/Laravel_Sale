f684354c80a4dc43f02d561eaaecc66c578cb571
GiangNgTT <nguyenthithuygiang.tienphuoc@gmail.com>
2022-07-20 08:01:35 +0700

    first commit

---
+++ app/Http/Controllers/PageController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Http\Facades\Date;
+use App\Models\Product;
+use App\Models\ProductType;
+use App\Models\Slide;
+
+
+class PageController extends Controller
+{
+    public function index(){
+        $slide = Slide ::all();
+        $products = Product::simplePaginate();
+        $new_products  = Product::where('new', 1)->paginate(4);
+        return view('banhang.index', compact('slide','new_products', 'products'));
+    }
+    public function getLoaiSp($type){
+        $loai_sp = ProductType::all();
+        $sp_theoloai = Product::where('id_type',$type)->get();
+        // $sp_khac =  Product::where ('id_type','<>',$type)->paginate(3);
+        return view ('banhang.loai_sanpham',compact('sp_theoloai', 'loai_sp'));
+    }
+
+}
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\ProductType;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        view()->composer('layout.header', function ($view) {
+            $loai_sp = ProductType::all();
+            $view->with('loai_sp', $loai_sp);
+        });
+    }
+}
